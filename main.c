f8060eb0fd8b2089439152c28667637952cbe909
KyleWyckoff <105688322+KyleWyckoff@users.noreply.github.com>
2025-02-15 14:52:26 -0800

    Create main.c
    I believe this is the functionality but please lmk if I am not interpretting a part of the directions correctly

---
+++ main.c
@@ -0,0 +1,16 @@
+
+
+
+int main (int argc, char *argv[]){
+//call FileCopy function
+//FileCopy(int argc, char *argv[])
+// takes in inputs of main and opens the file given, if any
+//creates a pipe and copies the file contents into the pipe
+FileCopy(argc, *argv[]);
+
+  
+//call child function
+//use pipe called in FileCopy and read the data stored in 
+//in order to write back out to a new text file
+
+}
