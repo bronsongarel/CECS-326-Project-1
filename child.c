52273979396d2a41c8ca6c517e8e04312606eecb
Bronson Garel <78556724+bronsongarel@users.noreply.github.com>
2025-02-15 22:54:23 -0800

    Update child.c

--- child.c
+++ child.c
@@ -4,12 +4,58 @@
  * This program reads the contents of the pipe and writes it to a file.
  */
   
-#include <stdio.h>
-#include <windows.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <windows.h>
+ #include <unistd.h>
+ #include <fcntl.h>
+ 
+ #define BUFFER_SIZE 10
+ 
+ int main(int argc, char *argv[])
+ {
+    if (argc != 2){
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
 
-#define BUFFER_SIZE 10
+    int copy = open(argv[1], O_RDONLY);
+    if (copy == -1) {
+        perror("Error opening file to copy");
+        return 1;
+    }
 
-void child(void)
-{
-    // Your code is here
-}
+    int destination = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0644);
+    if (destination == -1) {
+        perror("Error opening copy destination file");
+        close(copy);
+        return 1;
+    }
+
+    // Assuming pipe is created
+
+    // Fork Child process
+    pid_t pid = fork();
+    if (pid == -1){
+        perror("Fork not created");
+        close(copy);
+        close(destination);
+        return 1;
+    }
+
+
+    // Child process
+    if (pid == 0){
+        // NEED TO ClOSE WRITE END OF PIPE (PIPE[1])
+        char buf[BUFFER_SIZE];
+        ssize_t read_b;
+        while ((read_b = read(/*PIPE FIRST INDEX*/, buf, sizeof(buf))) > 0) {
+            write(destination, buf, read_b);
+        }
+        close(/*PIPE FIRST INDEX*/);
+        close(destination);
+        exit(0);
+    }
+    // else, parent function read copy file and write to pipe
+ }
+ 
