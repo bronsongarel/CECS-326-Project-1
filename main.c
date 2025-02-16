b422c7cfe8728b8df18ce53b7c92144d9c776f89
KyleWyckoff <105688322+KyleWyckoff@users.noreply.github.com>
2025-02-16 12:20:21 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,61 @@
+#include <windows.h>
+#include <stdio.h>
 
 
 
-int main (int argc, char *argv[]){
-//call FileCopy function
-//FileCopy(int argc, char *argv[])
-// takes in inputs of main and opens the file given, if any
-//creates a pipe and copies the file contents into the pipe
-FileCopy(argc, *argv[]);
+#define BUFFER_SIZE 4096  // Buffer size for reading/writing
+#define STRING_SIZE 25
+void FileCopy(HANDLE hWritePipe, const char *inputFile);
+void child(HANDLE hReadPipe, const char *outputFile);
 
-  
-//call child function
-//use pipe called in FileCopy and read the data stored in 
-//in order to write back out to a new text file
+int main(int argc, char *argv[]) {
+    if(argc < 3){
+        perror("Error with text files passed");
+        return 1;
+     }
+    HANDLE hReadPipe, hWritePipe;  //handles for reading and writing to our pipe
+    SECURITY_ATTRIBUTES sa = { sizeof(SECURITY_ATTRIBUTES), NULL, TRUE };  // Allows inheritance
+    if(!CreatePipe(&hReadPipe, &hWritePipe, &sa, BUFFER_SIZE)){ 
+        perror("The Pipe was not Created");
+        return 0;
+    }
+     FileCopy(hWritePipe, argv[1]);      // Write file contents to the pipe
+     CloseHandle(hWritePipe);            // Close the write end of the pipe before reading
+     child(hReadPipe, argv[2]);          // Read from the pipe and write to output file
+     CloseHandle(hReadPipe);             // Close the read end of the pipe
+     printf("File successfully copied through the pipe!\n");
+     return 0;
+ }
 
-}
+ void FileCopy(HANDLE hWritePipe, const char *inputFile){ 
+    FILE *stream = fopen(inputFile, "r");  // Open file in read mode
+    if (stream == NULL) {
+       perror("Error opening file");
+       return;
+    }
+    DWORD written;
+    char buffer[BUFFER_SIZE];
+    while(fgets(buffer, sizeof(buffer), stream)){           //Write to the pipe from input file
+       if(!WriteFile(hWritePipe, buffer, strlen(buffer), &written, NULL)){
+          perror("Error when writing to the string");
+          break;
+       }
+    
+    } 
+    fclose(stream); //Close the file
+    return;
+ }
+
+
+ void child(HANDLE hReadPipe, const char *outputFile){
+    FILE *output = fopen(outputFile, "w"); // Open in text mode
+    if (!output) {
+        perror("Error opening output file");
+        return;
+    }
+    char buffer[BUFFER_SIZE];
+    DWORD bytesRead;
+    while (ReadFile(hReadPipe, buffer, BUFFER_SIZE, &bytesRead, NULL) && bytesRead > 0) {
+        fwrite(buffer, 1, bytesRead, output);
+    }
+    fclose(output);}
